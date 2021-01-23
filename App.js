5e33a3c10db2e1c41aee0bb04ad5c6f8188c2d6e
dewanggaaw <dewanggaa44@gmail.com>
2021-01-23 13:27:26 +0700

    dependencies installed

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {
   SafeAreaView,
